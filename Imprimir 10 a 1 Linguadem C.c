1a3988cac352ccfd8ef2b1577b065912f9367e67
ArthurRiosDomingos <159266615+ArthurRiosDomingos@users.noreply.github.com>
2025-02-26 08:06:29 -0300

    Add files via upload

---
+++ Imprimir 10 a 1 Linguadem C.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(){
+    int n = 10;
+    
+    printf("números: %d", n);
+    while(n!=1){
+        n = n - 1;
+        printf("\ntexto: %d", n);
+    }
+    
+    if(n = 1){
+        n = n - 1;
+        printf("\nnúmero: %d", n);
+    }
+    
+
+    return 0;
+}
